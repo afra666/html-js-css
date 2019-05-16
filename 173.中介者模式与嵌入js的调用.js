416238423e0da95c40ad25e864b084274edb95a6
afra666 <48674984+afra666@users.noreply.github.com>
2019-05-16 09:28:29 +0800

    Create 173.中介者模式与嵌入js的调用.js

---
+++ 173.中介者模式与嵌入js的调用.js
@@ -0,0 +1,53 @@
+<!--1/3 index.html-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <style type="text/css">
+        canvas{
+            border: 1px solid yellow;
+        }
+    </style>
+</head>
+<body>
+<canvas id="myCanvas"></canvas>
+<script src="js/Game.js"></script>
+<script src="js/Bird.js"></script>
+<script src="js/Background.js"></script>
+<script src="js/Pipe.js"></script>
+<script>
+    //不能加window.onload,否则Pipe.js中无法识别game
+    let game=new Game("123");
+    new Pipe();
+
+</script>
+</body>
+</html>
+
+
+
+//2.Game.js
+(function () {
+    //let Game供本js内部使用,外部无法调用:Game.prototype.x=6;
+    //window.Game供html使用:new Game("123")
+    let Game=window.Game=function (idStr) {
+    };
+    Game.prototype.start=function () {
+       console.log("start");
+    };
+    Game.prototype.end=function () {
+       console.log("end");
+    };
+})();
+
+
+//3.Pipe.js
+(function () {
+    let Pipe=window.Pipe=function () {
+        //game来自html中的嵌入js
+       game.start();
+       game.end();
+    }
+})();
+
